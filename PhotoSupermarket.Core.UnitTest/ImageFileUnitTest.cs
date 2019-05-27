12075924d3e19d701e3502585d03eebf213a6d21
tinyAdapter <tinyadapter@outlook.com>
2019-05-27 14:57:39 +0800

    添加BmpImageParser类
    解析过程都写在这里

--- PhotoSupermarket.Core.UnitTest/ImageFileUnitTest.cs
+++ PhotoSupermarket.Core.UnitTest/ImageFileUnitTest.cs
@@ -8,7 +8,7 @@ namespace PhotoSupermarket.Core.UnitTest
         [Fact]
         public void TestLoadBmpImage()
         {
-            var image = Util.ImageFile.LoadBmpImage(".\\TestImages\\lenna.bmp");
+            var image = Util.ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna.bmp");
             Assert.Equal(0x4D42, image.FileHeader.Type);
             Assert.Equal(786486u, image.FileHeader.Size);
             Assert.Equal(0, image.FileHeader.Reserved1);
--- PhotoSupermarket.Core/Util/ImageFile.cs
+++ PhotoSupermarket.Core/Util/ImageFile.cs
@@ -7,17 +7,68 @@ namespace PhotoSupermarket.Core.Util
     {
         public static BmpImage LoadBmpImage(string filePath)
         {
-            int currentIndex = 0;
-
-            BmpImage image = new BmpImage();
-            byte[] allImageBytes = File.ReadAllBytes(filePath);
-            if (Bytes.ReadUShort(allImageBytes, ref currentIndex) != 0x4D42)
-                throw new NotValidImageFileException();
-
-            return image;
+            BmpImageParser parser = new BmpImageParser(File.ReadAllBytes(filePath));
+            parser.Parse();
+            return parser.Image;
         }
 
         public static bool SaveBmpImage(BmpImage image, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BmpImageParser
+    {
+        public BmpImageParser(byte[] imageBytes)
+        {
+            this.imageBytes = imageBytes;
+        }
+
+        private byte[] imageBytes;
+        private int currentIndex = 0;
+        public BmpImage Image { get; } = new BmpImage();
+
+        public void Parse()
+        {
+            ParseFileHeader();
+            //ParseInfoHeader();
+            //ParsePalette();
+            //ParseBitmapData();
+        }
+        
+        private void ParseFileHeader()
+        {
+            Image.FileHeader = new BitmapFileHeader();
+
+            Image.FileHeader = new BitmapFileHeader();
+            if ((Image.FileHeader.Type = Bytes.ReadUShort(imageBytes, ref currentIndex)) != 0x4D42)
+            {
+                throw new NotValidImageFileException();
+            }
+            Image.FileHeader.Size = Bytes.ReadUInt(imageBytes, ref currentIndex);
+            if ((Image.FileHeader.Reserved1 = Bytes.ReadUShort(imageBytes, ref currentIndex)) != 0)
+            {
+                throw new NotValidImageFileException();
+            }
+            if ((Image.FileHeader.Reserved2 = Bytes.ReadUShort(imageBytes, ref currentIndex)) != 0)
+            {
+                throw new NotValidImageFileException();
+            }
+            Image.FileHeader.OffBits = Bytes.ReadUInt(imageBytes, ref currentIndex);
+        }
+
+        private void ParseInfoHeader()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ParsePalette()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ParseBitmapData()
         {
             throw new NotImplementedException();
         }
