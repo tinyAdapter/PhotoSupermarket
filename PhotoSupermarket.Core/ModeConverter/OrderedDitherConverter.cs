33ba35ef53fc0152aea1aece6e8c5e72b0c40c87
caoyu <caoyupersonal@hotmail.com>
2019-06-03 17:01:15 +0800

    添加OrderedDitheringConverter的单元测试
    测试通过

--- PhotoSupermarket.Core/ModeConverter/OrderedDitherConverter.cs
+++ PhotoSupermarket.Core/ModeConverter/OrderedDitherConverter.cs
@@ -6,16 +6,20 @@ using System.Text;
 namespace PhotoSupermarket.Core.ModeConverter
 {
 
-    class OrderedDitherConverter : GrayToBinaryConverter
+    public class OrderedDitherConverter : GrayToBinaryConverter
     {
         int[,] ditherMatrix;
+        int ditherMatrixSize;
+
         public OrderedDitherConverter(BmpImage data) : base(data)
         {
             ditherMatrix = Dither.GenerateDitherMatrix(1);
+            ditherMatrixSize = 2;
         }
         public OrderedDitherConverter(BmpImage data, int DitherMatrixPower) : base(data)
         {
             ditherMatrix = Dither.GenerateDitherMatrix(DitherMatrixPower);
+            ditherMatrixSize = (int)Math.Pow(2, DitherMatrixPower);
         }
 
         protected override void ConvertBitmapData()
@@ -27,7 +31,7 @@ namespace PhotoSupermarket.Core.ModeConverter
             {
                 for (int y = 0; y < oldData.Height; y++)
                 {
-                    remapedOldData[x, y] = (int)(oldData.Get8BitDataAt(x, y) / (Math.Pow(ditherMatrix.Length, 2) - 1));
+                    remapedOldData[x, y] = (int)(oldData.Get8BitDataAt(x, y) / ditherMatrixSize - 1);
                 }
             }
 
@@ -35,12 +39,15 @@ namespace PhotoSupermarket.Core.ModeConverter
             int i, j;
             for (int x = 0; x < oldData.Width; x++)
             {
-                i = x % ditherMatrix.Length;
+                i = x % ditherMatrixSize;
                 for (int y = 0; y < oldData.Height; y++)
                 {
-                    j = y % ditherMatrix.Length;
+                    j = y % ditherMatrixSize;
+
                     if (remapedOldData[x, y] > ditherMatrix[i, j])
+                    {
                         Image.Data.Set1BitDataAt(x, y, true);
+                    }
                     else
                         Image.Data.Set1BitDataAt(x, y, false);
                 }
