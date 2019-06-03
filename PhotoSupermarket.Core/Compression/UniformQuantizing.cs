7b80823df28dce00a0ea2206a1c95fe8f3ee90ba
alo1719 <1335250440@qq.com>
2019-06-03 19:35:11 +0800

    初始化均匀量化

---
+++ PhotoSupermarket.Core/Compression/UniformQuantizing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoSupermarket.Core.Compression
+{
+    class UniformQuantizing
+    {
+        public BmpImage Image { get; private set; }
+        protected BitmapData oldData;
+        protected double QuantizedInterval;
+
+        public double[,] UniformQuantizingData { get; private set; }
+
+        public UniformQuantizing(BmpImage image, double CompressRatio)
+        {
+            QuantizedInterval = 256.0 / CompressRatio;
+            Image = image;
+            oldData = (BitmapData)Image.Data.Clone();
+        }
+    }
+}
