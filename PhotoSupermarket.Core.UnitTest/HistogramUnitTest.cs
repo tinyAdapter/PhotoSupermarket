75fceb46ccb3508daf8b14e2b8bb58b41fb698be
Alix00544 <439601575@qq.com>
2019-06-03 17:40:20 +0800

    暂存

---
+++ PhotoSupermarket.Core.UnitTest/HistogramUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PhotoSupermarket.Core.HistogramEqualization;
+using System.Collections.Generic;
+using Xunit;
+using System.Text;
+
+namespace PhotoSupermarket.Core.UnitTest
+{
+    public class HistogramUnitTest
+    {
+        [Fact]
+        public void TestGrayEqualization()
+        {
+            var image = Util.ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\直方图均衡测试图像gray1.bmp");
+            image = new GrayEqualization(image).Equalization().Image;
+            Util.ImageFile.SaveBmpImage(image,"..\\..\\..\\TestImages\\直方图均衡测试图像gray1_test.bmp");
+        }
+    }
+}
--- PhotoSupermarket.Core/BmpImage.cs
+++ PhotoSupermarket.Core/BmpImage.cs
@@ -190,6 +190,11 @@ namespace PhotoSupermarket.Core
         {
             return this.MemberwiseClone();
         }
+
+        internal void Set8BitDataAt(int i, int k, int v)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public enum BitmapColorMode
