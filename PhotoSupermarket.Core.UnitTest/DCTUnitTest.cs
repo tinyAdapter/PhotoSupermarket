df1c89826914cf6913de48771110ab50b14d3d7b
tinyAdapter <tinyadapter@outlook.com>
2019-06-03 18:08:58 +0800

    DCT变换 + 单元测试

---
+++ PhotoSupermarket.Core.UnitTest/DCTUnitTest.cs
@@ -0,0 +1,96 @@
+﻿using PhotoSupermarket.Core.Compression;
+using PhotoSupermarket.Core.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace PhotoSupermarket.Core.UnitTest
+{
+    public class DCTUnitTest
+    {
+        [Fact]
+        public void TestGenerateDCTBlock()
+        {
+            int[,] block = new int[4, 4]
+            {
+                { 42, 66, 68, 66 },
+                { 92, 4 , 76, 17 },
+                { 79, 85, 74, 71 },
+                { 96, 93, 39, 3  }
+            };
+            var matrix = DCTCompression.GenerateDCTBlock(block);
+            var shouldBe = new double[4, 4]
+            {
+                { 242.7500,  48.4317,  -9.7500,  23.5052 },
+                { -12.6428, -54.0659,   7.4278,  22.7950 },
+                {  -6.2500,  10.7158, -19.7500, -38.8046 },
+                {  40.6852, -38.7050, -11.4653, -45.9341 }
+            };
+            for (int i = 0; i < 4; i++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    Assert.Equal(shouldBe[i, k], matrix[i, k], 4);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestGenerateDCTReverseBlock()
+        {
+            double[,] block = new double[4, 4]
+            {
+                { 242.7500,  48.4317,  -9.7500,  23.5052 },
+                { -12.6428, -54.0659,   7.4278,  22.7950 },
+                {  -6.2500,  10.7158, -19.7500, -38.8046 },
+                {  40.6852, -38.7050, -11.4653, -45.9341 }
+            };
+            var matrix = DCTCompression.GenerateDCTReverseBlock(block);
+            var shouldBe = new int[4, 4]
+            {
+                { 42, 66, 68, 66 },
+                { 92, 4 , 76, 17 },
+                { 79, 85, 74, 71 },
+                { 96, 93, 39, 3  }
+            };
+            for (int i = 0; i < 4; i++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    Assert.Equal(shouldBe[i, k], matrix[i, k]);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestGetDCTImage()
+        {
+            var image = ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray.bmp");
+            BmpImage dctImage = new DCTCompression(image, 15).GetDCTImage();
+            ImageFile.SaveBmpImage(dctImage, "..\\..\\..\\TestImages\\lenna_dct_modified.bmp");
+        }
+
+        [Fact]
+        public void TestGetDCTReverseImage()
+        {
+            var image = ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray.bmp");
+            var dct = new DCTCompression(image, 15);
+            BmpImage dctImage = dct.GetDCTImage();
+            ImageFile.SaveBmpImage(dctImage, "..\\..\\..\\TestImages\\lenna_dct_modified.bmp");
+            BmpImage reverseImage = dct.GetDCTReverseImage(1.0);
+            ImageFile.SaveBmpImage(reverseImage, "..\\..\\..\\TestImages\\lenna_dct_reverse_modified.bmp");
+        }
+
+        [Fact]
+        public void TestGetDCTReverseImageWithCompression()
+        {
+            var image = ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray.bmp");
+            var dct = new DCTCompression(image, 15);
+            BmpImage dctImage = dct.GetDCTImage();
+            ImageFile.SaveBmpImage(dctImage, "..\\..\\..\\TestImages\\lenna_dct_comp_modified.bmp");
+            BmpImage reverseImage = dct.GetDCTReverseImage(8.0);
+            ImageFile.SaveBmpImage(reverseImage, "..\\..\\..\\TestImages\\lenna_dct_reverse_comp_modified.bmp");
+        }
+    }
+}
---
+++ PhotoSupermarket.Core/Compression/DCTCompression.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoSupermarket.Core.Compression
+{
+    public class DCTCompression
+    {
+        public BmpImage Image { get; private set; }
+        public int BlockSize { get; private set; }
+        protected BitmapData oldData;
+
+        public double[,] DCTData { get; private set; }
+
+        public DCTCompression(BmpImage image, int blockSize)
+        {
+            Image = image;
+            oldData = (BitmapData)Image.Data.Clone();
+            BlockSize = blockSize;
+        }
+
+        public BmpImage GetDCTImage()
+        {
+            if (Image.Data.ColorMode != BitmapColorMode.TwoFiftySixColors)
+            {
+                throw new NotThisColorModeException();
+            }
+
+            BmpImage result = NewGrayScaleImage(Image.Data.Width, Image.Data.Height);
+            DCTData = new double[Image.Data.Width, Image.Data.Height];
+
+            for (int i = 0; i < Image.Data.Width / BlockSize; i++)
+            {
+                for (int k = 0; k < Image.Data.Height / BlockSize; k++)
+                {
+                    int[,] block = new int[BlockSize, BlockSize];
+                    for (int m = 0; m < BlockSize; m++)
+                    {
+                        for (int n = 0; n < BlockSize; n++)
+                        {
+                            block[m, n] = Image.Data.Get8BitDataAt(BlockSize * i + m, BlockSize * k + n);
+                        }
+                    }
+                    double[,] dctBlock = GenerateDCTBlock(block);
+                    for (int m = 0; m < BlockSize; m++)
+                    {
+                        for (int n = 0; n < BlockSize; n++)
+                        {
+                            result.Data.Set8BitDataAt(
+                                BlockSize * i + m, BlockSize * k + n,
+                                (byte)((dctBlock[m, n] + 128) / 2));
+                            DCTData[BlockSize * i + m, BlockSize * k + n] = dctBlock[m, n];
+                        }
+                    }
+                }
+            }
+            
+            return result;
+        }
+
+        public BmpImage GetDCTReverseImage(double compressRatio)
+        {
+            if (Image.Data.ColorMode != BitmapColorMode.TwoFiftySixColors)
+            {
+                throw new NotThisColorModeException();
+            }
+            if (DCTData == null) throw new UnauthorizedAccessException();
+            if (compressRatio < 1.0) throw new ArgumentOutOfRangeException();
+
+            BmpImage result = NewGrayScaleImage(Image.Data.Width, Image.Data.Height);
+
+            for (int i = 0; i < Image.Data.Width / BlockSize; i++)
+            {
+                for (int k = 0; k < Image.Data.Height / BlockSize; k++)
+                {
+                    double[,] block = new double[BlockSize, BlockSize];
+                    for (int m = 0; m < BlockSize; m++)
+                    {
+                        for (int n = 0; n < BlockSize; n++)
+                        {
+                            if ((m + n > BlockSize * BlockSize / (compressRatio * compressRatio)))
+                            {
+                                block[m, n] = 0;
+                            }
+                            else
+                            {
+                                block[m, n] = DCTData[BlockSize * i + m, BlockSize * k + n];
+                            }
+                        }
+                    }
+                    int[,] dctReverseBlock = GenerateDCTReverseBlock(block);
+                    for (int m = 0; m < BlockSize; m++)
+                    {
+                        for (int n = 0; n < BlockSize; n++)
+                        {
+                            result.Data.Set8BitDataAt(
+                                BlockSize * i + m, BlockSize * k + n,
+                                (byte)dctReverseBlock[m, n]);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private BmpImage NewGrayScaleImage(int width, int height)
+        {
+            BmpImage result = new BmpImage()
+            {
+                Palette = new BitmapPaletteEntry[256]
+            };
+            for (int i = 0; i < 256; i++)
+            {
+                result.Palette[i] = new BitmapPaletteEntry((byte)i, (byte)i, (byte)i, 0);
+            }
+            result.Data = new BitmapData
+            {
+                ColorMode = BitmapColorMode.TwoFiftySixColors,
+                Width = width,
+                Height = height
+            };
+            result.Data.Data = new byte[result.Data.GetRealSize()];
+
+            return result;
+        }
+
+
+        public static double[,] GenerateDCTBlock(int[,] originalBlock)
+        {
+            int M = originalBlock.GetLength(0);
+            int N = originalBlock.GetLength(1);
+            double[,] result = new double[M, N];
+            
+            for (int u = 0; u < M; u++)
+            {
+                for (int v = 0; v < N; v++)
+                {
+                    result[u, v] = Alpha(u) * Alpha(v) / Math.Sqrt(M * N);
+                    double aggregate = 0;
+                    for (int x = 0; x < M; x++)
+                    {
+                        for (int y = 0; y < N; y++)
+                        {
+                            aggregate += 
+                                Math.Cos((2 * x + 1) * u * Math.PI / (2 * N)) *
+                                Math.Cos((2 * y + 1) * v * Math.PI / (2 * M)) *
+                                originalBlock[x, y];
+                        }
+                    }
+                    result[u, v] *= aggregate;
+                }
+            }
+
+            return result;
+        }
+
+        public static int[,] GenerateDCTReverseBlock(double[,] originalBlock)
+        {
+            int M = originalBlock.GetLength(0);
+            int N = originalBlock.GetLength(1);
+            int[,] result = new int[M, N];
+
+            double tempResult;
+            for (int x = 0; x < M; x++)
+            {
+                for (int y = 0; y < N; y++)
+                {
+                    tempResult = 1 / Math.Sqrt(M * N);
+                    double aggregate = 0;
+                    for (int u = 0; u < M; u++)
+                    {
+                        for (int v = 0; v < N; v++)
+                        {
+                            aggregate +=
+                                Alpha(u) * Alpha(v) *
+                                Math.Cos((2 * x + 1) * u * Math.PI / (2 * N)) *
+                                Math.Cos((2 * y + 1) * v * Math.PI / (2 * M)) *
+                                originalBlock[u, v];
+                        }
+                    }
+                    tempResult *= aggregate;
+                    result[x, y] = (int)Math.Round(tempResult);
+                }
+            }
+
+            return result;
+        }
+
+        private static double Alpha(int u)
+        {
+            if (u == 0) return 1;
+            else return Math.Sqrt(2);
+        }
+    }
+}
