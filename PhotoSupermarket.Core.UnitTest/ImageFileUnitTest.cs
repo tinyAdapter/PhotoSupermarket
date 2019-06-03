bbc2fe3d47e5c47e1e7c54c1d34e19178a2f5eb4
tinyAdapter <tinyadapter@outlook.com>
2019-06-03 14:04:03 +0800

    添加读取灰度图测试

--- PhotoSupermarket.Core.UnitTest/ImageFileUnitTest.cs
+++ PhotoSupermarket.Core.UnitTest/ImageFileUnitTest.cs
@@ -8,7 +8,7 @@ namespace PhotoSupermarket.Core.UnitTest
     public class ImageFileUnitTest
     {
         [Fact]
-        public void TestLoadBmpImage()
+        public void TestLoadRGBBmpImage()
         {
             var image = Util.ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna.bmp");
             //ParseFileHeader
@@ -30,6 +30,41 @@ namespace PhotoSupermarket.Core.UnitTest
             Assert.Equal(0, image.InfoHeader.YPelsPerMeter);
             Assert.Equal(0u, image.InfoHeader.ClrUsed);
             Assert.Equal(0u, image.InfoHeader.ClrImportanet);
+        }
+
+        [Fact]
+        public void TestLoadGrayBmpImage()
+        {
+            var image = Util.ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray.bmp");
+            //ParseFileHeader
+            Assert.Equal(0x4D42, image.FileHeader.Type);
+            Assert.Equal(91080u, image.FileHeader.Size);
+            Assert.Equal(0, image.FileHeader.Reserved1);
+            Assert.Equal(0, image.FileHeader.Reserved2);
+            Assert.Equal(1078u, image.FileHeader.OffBits);
+
+            //ParseInfoHeader
+            Assert.Equal(40u, image.InfoHeader.Size);
+            Assert.Equal(300, image.InfoHeader.Width);
+            Assert.Equal(300, image.InfoHeader.Height);
+            Assert.Equal(1u, image.InfoHeader.Planes);
+            Assert.Equal(8u, image.InfoHeader.BitCount);
+            Assert.Equal(0u, image.InfoHeader.Compression);
+            Assert.Equal(90002u, image.InfoHeader.SizeImage);
+            Assert.Equal(2834, image.InfoHeader.XPelsPerMeter);
+            Assert.Equal(2834, image.InfoHeader.YPelsPerMeter);
+            Assert.Equal(0u, image.InfoHeader.ClrUsed);
+            Assert.Equal(0u, image.InfoHeader.ClrImportanet);
+
+            // Palette
+            Assert.Equal(256, image.Palette.Length);
+            for (int i = 0; i < image.Palette.Length; i++)
+            {
+                Assert.Equal(i, image.Palette[i].Red);
+                Assert.Equal(i, image.Palette[i].Green);
+                Assert.Equal(i, image.Palette[i].Blue);
+                Assert.Equal(0, image.Palette[i].Flags);
+            }
         }
 
         [Fact]
