3be9b4e2e2dc7a0a97320d90457e8d976bd66e28
caoyu <caoyupersonal@hotmail.com>
2019-06-03 17:16:53 +0800

    对模式转换的单元测试代码冲突进行修改

--- PhotoSupermarket.Core.UnitTest/ModeConverterUnitTest.cs
+++ PhotoSupermarket.Core.UnitTest/ModeConverterUnitTest.cs
@@ -121,8 +121,8 @@ namespace PhotoSupermarket.Core.UnitTest
         {
             var image = Util.ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna.bmp");
             image = new YCbCrConverter(image).Convert().Image;
-            Util.ImageFile.SaveBmpImage(image, "..\\..\\..\\TestImages\\lenna_gray_ycbcr_modified.bmp");
-            image = Util.ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray_ycbcr_modified.bmp");
+            Util.ImageFile.SaveBmpImage(image, "..\\..\\..\\TestImages\\lenna_gray_modified.bmp");
+            image = Util.ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray_modified.bmp");
             // File header
             Assert.Equal(0x4D42, image.FileHeader.Type);
             Assert.Equal((uint)(14 + 40 + 256 * 4 + 512 * 512), image.FileHeader.Size);
@@ -178,14 +178,6 @@ namespace PhotoSupermarket.Core.UnitTest
             Assert.Equal(0u, image.InfoHeader.ClrUsed);
             Assert.Equal(0u, image.InfoHeader.ClrImportanet);
             // Palette
-            Assert.Equal(256, image.Palette.Length);
-            for (int i = 0; i < image.Palette.Length; i++)
-            {
-                Assert.Equal(i, image.Palette[i].Red);
-                Assert.Equal(i, image.Palette[i].Green);
-                Assert.Equal(i, image.Palette[i].Blue);
-                Assert.Equal(0, image.Palette[i].Flags);
-            }
             Assert.Equal(0, image.Palette[0].Red);
             Assert.Equal(0, image.Palette[0].Green);
             Assert.Equal(0, image.Palette[0].Blue);
