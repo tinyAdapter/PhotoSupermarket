087aed09aaeddefa9ee70881d0dff6742891c9c3
ukcm <838982156@qq.com>
2019-06-03 16:35:53 +0800

    控制台+YCbCr

--- PhotoSupermarket.Core.UnitTest/ModeConverterUnitTest.cs
+++ PhotoSupermarket.Core.UnitTest/ModeConverterUnitTest.cs
@@ -115,5 +115,41 @@ namespace PhotoSupermarket.Core.UnitTest
             Assert.Equal(255, image.Palette[1].Blue);
             Assert.Equal(0, image.Palette[1].Flags);
         }
+
+        [Fact]
+        public void TestYCbCrConverter()
+        {
+            var image = Util.ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna.bmp");
+            image = new YCbCrConverter(image).Convert().Image;
+            Util.ImageFile.SaveBmpImage(image, "..\\..\\..\\TestImages\\lenna_gray_ycbcr_modified.bmp");
+            image = Util.ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray_ycbcr_modified.bmp");
+            // File header
+            Assert.Equal(0x4D42, image.FileHeader.Type);
+            Assert.Equal((uint)(14 + 40 + 256 * 4 + 512 * 512), image.FileHeader.Size);
+            Assert.Equal(0, image.FileHeader.Reserved1);
+            Assert.Equal(0, image.FileHeader.Reserved2);
+            Assert.Equal((uint)(14 + 40 + 256 * 4), image.FileHeader.OffBits);
+            // Info header
+            Assert.Equal(40u, image.InfoHeader.Size);
+            Assert.Equal(512, image.InfoHeader.Width);
+            Assert.Equal(512, image.InfoHeader.Height);
+            Assert.Equal(1u, image.InfoHeader.Planes);
+            Assert.Equal(8u, image.InfoHeader.BitCount);
+            Assert.Equal(0u, image.InfoHeader.Compression);
+            Assert.Equal((uint)(512 * 512), image.InfoHeader.SizeImage);
+            Assert.Equal(0, image.InfoHeader.XPelsPerMeter);
+            Assert.Equal(0, image.InfoHeader.YPelsPerMeter);
+            Assert.Equal(0u, image.InfoHeader.ClrUsed);
+            Assert.Equal(0u, image.InfoHeader.ClrImportanet);
+            // Palette
+            Assert.Equal(256, image.Palette.Length);
+            for (int i = 0; i < image.Palette.Length; i++)
+            {
+                Assert.Equal(i, image.Palette[i].Red);
+                Assert.Equal(i, image.Palette[i].Green);
+                Assert.Equal(i, image.Palette[i].Blue);
+                Assert.Equal(0, image.Palette[i].Flags);
+            }
+        }
     }
 }
---
+++ PhotoSupermarket.Core/ModeConverter/YCbCrConverter.cs
@@ -0,0 +1,21 @@
+﻿using PhotoSupermarket.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoSupermarket.Core.ModeConverter
+{
+    public class YCbCrConverter : ColorToGrayConverter
+    {
+        public YCbCrConverter(BmpImage data) : base(data)
+        {
+        }
+
+        protected override byte GetGrayValueAt(int x, int y)
+        {
+            RGB oldValue = oldData.GetRGBDataAt(x, y);
+            //Y = 0.257*R+0.564*G+0.098*B+16
+            return (byte)((oldValue.R * 0.257) + (oldValue.G * 0.564) + (oldValue.B * 0.098) + 16);
+        }
+    }
+}
