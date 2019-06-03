d4bc934d0b37246cda0061790b26bcea6133ac4d
alo1719 <1335250440@qq.com>
2019-06-03 14:30:04 +0800

    构建GrayToBinaryConverter抽象类

---
+++ PhotoSupermarket.Core/ModeConverter/GrayToBinaryConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoSupermarket.Core.ModeConverter
+{
+    abstract class GrayToBinaryConverter : BaseConverter
+    {
+        public GrayToBinaryConverter(BmpImage data) : base(data)
+        {
+        }
+
+        protected override void DoConvert()
+        {
+            Image.Data.ColorMode = BitmapColorMode.MonoChrome;
+            GeneratePalette();
+            ConvertBitmapData();
+        }
+
+        private void GeneratePalette()
+        {
+            Image.Palette = new BitmapPaletteEntry[2];
+            Image.Palette[0] = new BitmapPaletteEntry((byte)0, (byte)0, (byte)0, 0);
+            Image.Palette[1] = new BitmapPaletteEntry((byte)255, (byte)255, (byte)255, 0);
+        }
+
+        protected abstract void ConvertBitmapData();
+    }
+}
