e31e3de665fe3d66ac505745c5564ea92ccc42e3
alo1719 <1335250440@qq.com>
2019-06-04 19:57:29 +0800

    实现可以自定义压缩比的均匀量化(与反量化)

---
+++ PhotoSupermarket.Core.UnitTest/UniformQuantizingUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using PhotoSupermarket.Core.Compression;
+using PhotoSupermarket.Core.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace PhotoSupermarket.Core.UnitTest
+{
+    public class UniformQuantizingUnitTest
+    {
+        [Fact]
+        public void TestUniformQuantizingCompress2()
+        {
+            var image = ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray.bmp");
+            UniformQuantizing uq = new UniformQuantizing(image, 2.0);
+            BmpImage uqImage = uq.InverseUniformQuantizaing();
+            ImageFile.SaveBmpImage(uqImage, "..\\..\\..\\TestImages\\lenna_uq2_modified.bmp");
+            Assert.Equal(16, uq.TotalIntervals);
+        }
+
+        [Fact]
+        public void TestUniformQuantizingCompress4()
+        {
+            var image = ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray.bmp");
+            UniformQuantizing uq = new UniformQuantizing(image, 4.0);
+            BmpImage uqImage = uq.InverseUniformQuantizaing();
+            ImageFile.SaveBmpImage(uqImage, "..\\..\\..\\TestImages\\lenna_uq4_modified.bmp");
+            Assert.Equal(4, uq.TotalIntervals);
+        }
+
+        [Fact]
+        public void TestUniformQuantizingCompress8()
+        {
+            var image = ImageFile.LoadBmpImage("..\\..\\..\\TestImages\\lenna_gray.bmp");
+            UniformQuantizing uq = new UniformQuantizing(image, 8.0);
+            BmpImage uqImage = uq.InverseUniformQuantizaing();
+            ImageFile.SaveBmpImage(uqImage, "..\\..\\..\\TestImages\\lenna_uq8_modified.bmp");
+            Assert.Equal(2, uq.TotalIntervals);
+        }
+    }
+}
--- PhotoSupermarket.Core/Compression/UniformQuantizing.cs
+++ PhotoSupermarket.Core/Compression/UniformQuantizing.cs
@@ -4,19 +4,119 @@ using System.Text;
 
 namespace PhotoSupermarket.Core.Compression
 {
-    class UniformQuantizing
+    public class UniformQuantizing
     {
-        public BmpImage Image { get; private set; }
-        protected BitmapData oldData;
-        protected double QuantizedInterval;
-
-        public double[,] UniformQuantizingData { get; private set; }
+        public BmpImage Image { get; }
+        public byte[,] UniformQuantizingData { get; private set; }
+        public int TotalIntervals { get; }
+        public double QuantizingInterval { get; }
 
         public UniformQuantizing(BmpImage image, double CompressRatio)
         {
-            QuantizedInterval = 256.0 / CompressRatio;
+            double intendingBit = 8.0 / CompressRatio;
+            double intendingTotalIntervals = Math.Pow(2, intendingBit);
+            QuantizingInterval = 256.0 / intendingTotalIntervals;
+            TotalIntervals = (int)Math.Ceiling(intendingTotalIntervals);
             Image = image;
-            oldData = (BitmapData)Image.Data.Clone();
+            UniformQuantizingCompress();
+        }
+
+        // the result bits of this method range from 1-8.
+        public void UniformQuantizingCompress()
+        {
+            if (Image.Data.ColorMode != BitmapColorMode.TwoFiftySixColors)
+            {
+                throw new NotThisColorModeException();
+            }
+
+            UniformQuantizingData = new byte[Image.Data.Width, Image.Data.Height];
+
+            for (int i = 0; i < Image.Data.Width; i++)
+            {
+                for (int j = 0; j < Image.Data.Height; j++)
+                {
+                    byte originalColor = Image.Data.Get8BitDataAt(i, j);
+                    byte newColor = (byte)(originalColor / QuantizingInterval);
+                    UniformQuantizingData[i, j] = newColor;
+                }
+            }
+        }
+
+        // this method only returns a bitmap with 1/8 bit(s).
+        public BmpImage InverseUniformQuantizaing()
+        {
+            if (UniformQuantizingData == null) throw new UnauthorizedAccessException();
+
+            if (TotalIntervals <= 2) return Inverse1BitBitmap();
+            else return Inverse8BitsBitmap();
+        }
+
+        private BmpImage Inverse8BitsBitmap()
+        {
+            BmpImage result = NewGrayScaleImage();
+
+            for (int i = 0; i < Image.Data.Width; i++)
+            {
+                for (int j = 0; j < Image.Data.Height; j++)
+                {
+                    result.Data.Set8BitDataAt(i, j, 
+                        (byte)((UniformQuantizingData[i, j] + 0.5) * QuantizingInterval));
+                }
+            }
+            return result;
+        }
+        
+        private BmpImage Inverse1BitBitmap()
+        {
+            BmpImage result = new BmpImage()
+            {
+                Palette = new BitmapPaletteEntry[2]
+            };
+            result.Palette[0] = new BitmapPaletteEntry((byte)0, (byte)0, (byte)0, 0);
+            result.Palette[1] = new BitmapPaletteEntry((byte)255, (byte)255, (byte)255, 0);
+            result.Data = new BitmapData
+            {
+                ColorMode = BitmapColorMode.MonoChrome,
+                Width = Image.Data.Width,
+                Height = Image.Data.Height
+            };
+            result.Data.Data = new byte[result.Data.GetRealSize()];
+            for (int i = 0; i < Image.Data.Width; i++)
+            {
+                for (int j = 0; j < Image.Data.Height; j++)
+                {
+                    if (UniformQuantizingData[i, j] == 0)
+                    {
+                        result.Data.Set1BitDataAt(i, j, false);
+                    }
+                    else
+                    {
+                        result.Data.Set1BitDataAt(i, j, true);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private BmpImage NewGrayScaleImage()
+        {
+            BmpImage result = new BmpImage()
+            {
+                Palette = new BitmapPaletteEntry[256]
+            };
+            for (int i = 0; i < 256; i++)
+            {
+                result.Palette[i] = new BitmapPaletteEntry((byte)i, (byte)i, (byte)i, 0);
+            }
+            result.Data = new BitmapData
+            {
+                ColorMode = BitmapColorMode.TwoFiftySixColors,
+                Width = Image.Data.Width,
+                Height = Image.Data.Height
+            };
+            result.Data.Data = new byte[result.Data.GetRealSize()];
+
+            return result;
         }
     }
 }
