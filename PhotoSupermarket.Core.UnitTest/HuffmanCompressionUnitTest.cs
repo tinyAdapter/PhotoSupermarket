938cf8840218940bd76616c2f9359f965157bd68
tinyAdapter <tinyadapter@outlook.com>
2019-06-10 14:00:19 +0800

    去除硬编码绝对路径

--- PhotoSupermarket.Core.UnitTest/HuffmanCompressionUnitTest.cs
+++ PhotoSupermarket.Core.UnitTest/HuffmanCompressionUnitTest.cs
@@ -15,7 +15,6 @@ namespace PhotoSupermarket.Core.UnitTest
             byte[] bytes = new byte[] {1,2,3,4,5,6,7,8};
             HuffmanCompression huff = new HuffmanCompression(bytes);
             huff.Compress("..\\..\\..\\TestImages\\huff_dictionary_test.bi");
-            //Assert.Equal(bytes.Length, File.ReadAllBytes("..\\..\\..\\TestImages\\huff_dictionary_test.bi")[0]);
         }
 
         [Fact]
@@ -23,9 +22,9 @@ namespace PhotoSupermarket.Core.UnitTest
         {
             byte[] bytes = new byte[] { 1, 1, 1, 2, 3, 23, 4, 65, 3, 5, 3, 6, 4, 78, 5, 9, 56, 5, 8, 5, 8, 4, 8, 43, 78, 4, 87 };
             HuffmanCompression huff = new HuffmanCompression(bytes);
-            huff.Compress("F:\\CourseFile\\MultimediaTechnology\\PhotoSupermarket\\PhotoSupermarket.Core.UnitTest\\TestImages\\huffman_compressed_data.bi");
-            byte[] storedBytes = File.ReadAllBytes("F:\\CourseFile\\MultimediaTechnology\\PhotoSupermarket\\PhotoSupermarket.Core.UnitTest\\TestImages\\huffman_compressed_data.bi");
-            HuffmanDecompression decompression = new HuffmanDecompression("F:\\CourseFile\\MultimediaTechnology\\PhotoSupermarket\\PhotoSupermarket.Core.UnitTest\\TestImages\\huffman_compressed_data.bi");
+            huff.Compress("..\\..\\..\\TestImages\\huffman_compressed_data.bi");
+            byte[] storedBytes = File.ReadAllBytes("..\\..\\..\\TestImages\\huffman_compressed_data.bi");
+            HuffmanDecompression decompression = new HuffmanDecompression("..\\..\\..\\TestImages\\huffman_compressed_data.bi");
             byte[] decompressedBytes = decompression.Decompress();
 
             Assert.Equal(bytes.Length, decompressedBytes.Length);
